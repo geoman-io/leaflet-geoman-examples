4f8967c8ddbbd93f15333452f3b5d14e218554bf
dailyprice <68426743+dailyprice@users.noreply.github.com>
2024-06-12 11:39:53 +0100

    Update App.tsx

--- leaflet-vite/src/App.tsx
+++ leaflet-vite/src/App.tsx
@@ -9,6 +9,10 @@ const App = () => {
       scrollWheelZoom={true}
       style={{ width: "100vw", height: "100vh" }}
     >
+      {/* 
+        Note: The TileLayer URL below is intended for use on localhost only.
+        Users need to generate their own API key from a tile provider (e.g. MapTiler) and replace the placeholder in the URL.
+      */}
       <TileLayer
         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=FTejVijNkqKWPbQui8i9"
